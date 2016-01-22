e9cd19991a8ee947ff01b1f7cf9bbe6ce4d10a54
nmnitinmohan <07nitinmohan@gmail.com>
2016-01-22 16:59:13 +0530

    initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingredients = [{"id":"1","text":"ham"},{"id":"2","text":"hamburger"}];
+
+var List= React.createClass({
+  render: function(){
+
+      var listItems = ingredients.map(function(item){
+          return <ListItem key = {item.id} ingredient = {item.text}></ListItem>;
+      });
+
+      return(
+        <ul> {listItems} </ul>
+      );
+
+  }
+});
+
+
+module.exports = List;
